2f8b0daba2ea9b957a8c03a7a1afc6be8e5e4733
Vincent COURTIN <vincent.courtin@link-value.fr>
2017-12-15 13:26:31 +0100

    wip script initializer

--- project_initialisation.sh
+++ project_initialisation.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-git clone
-
-mkdir ./nginx
-mkdir ./php
-
-cp Dockerfile_php.dist ./php/Dockerfile
+clear
+echo "Welcome to the initializer of the docker environment."
+echo "You are now going to set environment variable for all dockers containers."
+echo "Variables left empty wll be set to the default value in parenthesis"
+echo "Example : PROJECT_NAME('dockerphptest'):"
+echo "All Values must be without any white space."
